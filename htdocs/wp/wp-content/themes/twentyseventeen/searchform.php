7564e6b497edd44e819f3d0fa97243bcb6dca5a1
superdrumskills <grassyknollkid@gmail.com>
2017-07-10 23:06:22 -0600

    7/10 Backup
    Pushing Items into Github as a backup

--- htdocs/wp/wp-content/themes/twentyseventeen/searchform.php
+++ htdocs/wp/wp-content/themes/twentyseventeen/searchform.php
@@ -12,12 +12,14 @@
 
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="<?php echo $unique_id; ?>">
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentyseventeen' ); ?></span>
-	</label>
-	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyseventeen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-
+<form role="search" method="get" id="searchform" action="<?php bloginfo('siteurl'); ?>">
+  <div>
+     <label class="search-form" for="s">Search for:</label>
+    <input type="text" value="" name="s" id="s" /> 
+   &nbsp
+	<div margin-top: 20px;><?php wp_dropdown_categories( 'show_option_all=All Categories' ); ?> 
+   <input type="submit" id="searchsubmit" value="Search" margin-top: 20px float: right ;/> </div>
+  </div>
 </form>
-&nbsp;
-<?php echo do_shortcode( '[searchandfilter taxonomies="category"]'); ?>
+</form>
+
